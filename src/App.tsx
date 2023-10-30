c8d6aa1db39cc2776c382b5bba1d746b9478f720
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-30 09:20:12 +0100

    Add chocolate chip cookie component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import ButterBiscuit from "./ButterBiscuit.tsx";
+import ChocolateChipCookie from "./ChocolateChipCookie.tsx";
 
 function App() {
 
     return (
         <>
             <h1>Schoko-Laden</h1>
-            <ButterBiscuit/>
+            <ChocolateChipCookie/>
         </>
     )
 }
---
+++ src/ChocolateChipCookie.tsx
@@ -0,0 +1,11 @@
+export default function ChocolateChipCookie() {
+    return (
+        <>
+            Unsere Schokokekse:
+            <ul>
+                <li>Schokokeks mit Hafer</li>
+                <li>Schokokeks mit weißer Schokolade</li>
+            </ul>
+        </>
+    );
+}
