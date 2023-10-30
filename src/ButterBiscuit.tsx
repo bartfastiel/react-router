6531bc4063f308143214b3b5d3509b85ca993946
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-30 09:16:23 +0100

    Add butter biscuit component

---
+++ src/ButterBiscuit.tsx
@@ -0,0 +1,11 @@
+export default function ButterBiscuit() {
+    return (
+        <>
+            Unsere Butterkekse:
+            <ul>
+                <li>Butterkeks mit Nüssen</li>
+                <li>Butterkeks mit Brombeer-Geschmack</li>
+            </ul>
+        </>
+    );
+}
